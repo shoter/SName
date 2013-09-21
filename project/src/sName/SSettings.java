d1efc52d5d92dab037a2a99dc30a237b9827e348
Damian <damiansht@gmail.com>
2013-09-21 22:45:23 +0200

    added SSetings interface

---
+++ project/src/sName/SSettings.java
@@ -0,0 +1,23 @@
+package sName;
+
+public abstract class SSettings
+{
+	protected SNameManager manager;
+	public SSettings()
+	{
+		manager = SName.get().m_nameManager;
+	}
+	
+	/**
+	 * saves all things connected with plugin
+	 * @return true on success
+	 */
+	public abstract boolean save();
+	
+	/**
+	 * loads all things connected with plugin
+	 * manager should be empty
+	 * @return true on success
+	 */
+	public abstract boolean load();
+}
