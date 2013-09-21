362ff2c74289b94c33578babb2da073c6ad9400c
Damian <damiansht@gmail.com>
2013-09-21 22:47:50 +0200

    added empty settings class

---
+++ project/src/sName/SEmptySettings.java
@@ -0,0 +1,17 @@
+package sName;
+
+
+public class SEmptySettings extends SSettings
+{
+
+	@Override
+	public boolean save() {
+		return true;
+	}
+
+	@Override
+	public boolean load() {
+		return true;
+	}
+
+}
